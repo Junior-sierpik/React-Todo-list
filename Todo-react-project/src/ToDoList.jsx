3b5d351155cd35307f1d438a64f63efb90f9ae3e
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-07 16:35:04 +0200

    Create variables and functions

--- Todo-react-project/src/ToDoList.jsx
+++ Todo-react-project/src/ToDoList.jsx
@@ -2,6 +2,32 @@ import React, {useState} from "react"
 
 function ToDoList(){
 
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event){
+
+  }
+
+  function addTask(){
+
+  }
+
+  function deleteTask(index){
+
+  }
+
+  function moveTaskUp(index){
+
+  }
+
+  function moveTaskDown(index){
+    
+  }
+
+  return(
+    <></>
+  );
 }
 
 export default ToDoList
