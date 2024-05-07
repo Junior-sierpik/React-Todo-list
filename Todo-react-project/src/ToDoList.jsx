87a338915f85819834b9c983bd3eec1e361986b2
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-07 16:48:45 +0200

    Add a HTML to project

--- Todo-react-project/src/ToDoList.jsx
+++ Todo-react-project/src/ToDoList.jsx
@@ -2,11 +2,11 @@ import React, {useState} from "react"
 
 function ToDoList(){
 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk with dog"]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event){
-
+    setNewTask(event.target.value);
   }
 
   function addTask(){
@@ -22,11 +22,39 @@ function ToDoList(){
   }
 
   function moveTaskDown(index){
-    
+
   }
 
   return(
-    <></>
+    <div className="todo-list">
+      <h1>Todo List</h1>
+
+      <div>
+        <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}/>
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => 
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              ❌
+            </button>
+
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              👆
+            </button>
+
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              👇
+            </button>
+          </li>
+        )}
+      </ol>
+    </div>
   );
 }
 
