56d7d21d2ba8cd6b0c825169acbdcc85d0f2e05d
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-07 17:24:24 +0200

    You can now move tasks up and down 👌

--- Todo-react-project/src/ToDoList.jsx
+++ Todo-react-project/src/ToDoList.jsx
@@ -23,11 +23,23 @@ function ToDoList(){
   }
 
   function moveTaskUp(index){
+      if(index > 0){
+        const updatedTasks = [...tasks];
+        [updatedTasks[index], updatedTasks[index - 1]] =
+        [updatedTasks[index - 1], updatedTasks[index]];
 
+      setTasks(updatedTasks);
+      }
   }
 
   function moveTaskDown(index){
+    if(index < tasks.length - 1){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] =
+      [updatedTasks[index + 1], updatedTasks[index]];
 
+    setTasks(updatedTasks);
+    }
   }
 
   return(
